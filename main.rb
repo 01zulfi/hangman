39af1bb8ebf014a86fa66f34c4f3e1e80a669935
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-07-18 13:32:41 +0500

    Add Hangman class - Add initialize method - Add start public method - Add private methods: player_input, valid_character_input, game_end?, hanged?, saved?

--- main.rb
+++ main.rb
@@ -1,11 +1,64 @@
 # frozen_string_literal: true
 
-def random_word_from_dictionary
-  dictionary = []
-  file = File.read('dictionary.txt')
-  file.each_line do |line|
-    line.strip!
-    dictionary << line if line.length.between?(5, 12)
+# hangman class
+class Hangman
+  def initialize
+    @word = random_word_from_dictionary.split('').uniq
+    @guessed = false
+    @correct = []
+    @incorrect = []
   end
-  dictionary.sample
+
+  def start
+    until game_end?
+      move = player_input
+      if @word.include?(move)
+        @correct << move
+        @correct.uniq!
+      else
+        @incorrect << move
+        @incorrect.uniq!
+      end
+    end
+  end
+
+  private
+
+  def player_input
+    character = ''
+    character = gets.chomp until valid_character_input(character)
+    character
+  end
+
+  def valid_character_input(input)
+    correct_length = input.length == 1
+    already_used = @correct.include?(input) || @incorrect.include?(input)
+    only_alphabets = input.match?(/\A[a-zA-Z'-]*\z/)
+
+    correct_length && only_alphabets && !already_used
+  end
+
+  def random_word_from_dictionary
+    dictionary = []
+    file = File.read('dictionary.txt')
+    file.each_line do |line|
+      word = line.strip
+      dictionary << word if word.length.between?(5, 12)
+    end
+    dictionary.sample
+  end
+
+  def game_end?
+    hanged? || saved?
+  end
+
+  def hanged?
+    @incorrect.length == 6
+  end
+
+  def saved?
+    @correct.length == @word.length
+  end
 end
+
+Hangman.new.start
