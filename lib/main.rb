2ca4f6e1ade77ed3d04bfa808acd90fc14e01025
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-07-18 14:20:37 +0500

    Add Main class - to handle selection b/w new or load game

--- lib/main.rb
+++ lib/main.rb
@@ -2,14 +2,14 @@
 
 # hangman class
 class Hangman
-  def initialize
-    @word = random_word_from_dictionary.split('').uniq
-    @guessed = false
-    @correct = []
-    @incorrect = []
+  def initialize(word, correct = [], incorrect = [])
+    @word = word.split('').uniq
+    @correct = correct
+    @incorrect = incorrect
   end
 
   def start
+    p @word
     until game_end?
       move = player_input
       if @word.include?(move)
@@ -19,6 +19,8 @@ class Hangman
         @incorrect << move
         @incorrect.uniq!
       end
+      p "correct #{@correct}"
+      p "incorrect #{@incorrect}"
     end
   end
 
@@ -38,16 +40,6 @@ class Hangman
     correct_length && only_alphabets && !already_used
   end
 
-  def random_word_from_dictionary
-    dictionary = []
-    file = File.read('dictionary.txt')
-    file.each_line do |line|
-      word = line.strip
-      dictionary << word if word.length.between?(5, 12)
-    end
-    dictionary.sample
-  end
-
   def game_end?
     hanged? || saved?
   end
@@ -61,4 +53,40 @@ class Hangman
   end
 end
 
-Hangman.new.start
+# main class
+class Main
+  def start
+    if game_mode == '1'
+      Hangman.new(random_word_from_dictionary).start
+    else
+      Hangman.new('scrumptious', %w[s p], %w[z x l]).start
+    end
+  end
+
+  private
+
+  def game_mode
+    puts '1. new game'
+    puts '2. load game'
+
+    mode = ''
+    mode = gets.chomp until valid_mode(mode)
+    mode
+  end
+
+  def valid_mode(mode)
+    %w[1 2].include?(mode)
+  end
+
+  def random_word_from_dictionary
+    dictionary = []
+    file = File.read('dictionary.txt')
+    file.each_line do |line|
+      word = line.strip
+      dictionary << word if word.length.between?(5, 12)
+    end
+    dictionary.sample
+  end
+end
+
+Main.new.start
